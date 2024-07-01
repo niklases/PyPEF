aacafbb19630f736426473090ef90234c67620b2
niklases <60261497+niklases@users.noreply.github.com>
2024-07-01 13:33:31 +0200

    Skip dataset if no performance can be computed e.g., as all variants define substitutions at gap positions

--- scripts/ProteinGym_runs/run_performance_tests_proteingym_data.py
+++ scripts/ProteinGym_runs/run_performance_tests_proteingym_data.py
@@ -75,6 +75,7 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
                       f'amino acid substitutions at gap positions (these variants will be predicted/labeled with a fitness of 0.0).')
             if ratio_input_vars_at_gaps >= 1.0:
                 print('100% substitutions at gap sites, skipping dataset...')
+                continue
             #variants, sequences, fitnesses = remove_gap_pos(gaps, variants, sequences, fitnesses)
             x_dca = gremlin_new.collect_encoded_sequences(sequences)
             x_wt = gremlin_new.x_wt
