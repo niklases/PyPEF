1a7a0d24d671269439421d6e3bb89499197920b7
niklases <60261497+niklases@users.noreply.github.com>
2024-07-09 13:48:43 +0200

    Install PyPEF with pip in environment by default

--- scripts/Runtime_tests/os_system_runtimes.py
+++ scripts/Runtime_tests/os_system_runtimes.py
@@ -21,19 +21,21 @@ avgfp_path = os.path.abspath(
     )
 )
 
-# Assuming that the Conda environment 'pypef' exists and contains all necessary Python packages, 
-# using'avGFP_shortened.csv' instead of 'avGFP.csv' takes much less computing time
+# Assuming that the Conda environment 'pypef' exists and installs 
+# the PyPEF package into the pypef environment
+os.system("conda run -n pypef python -m pip install -U pypef")
+# Using'avGFP_shortened.csv' instead of 'avGFP.csv' takes much less computing time
 cmd = f"conda run -n pypef python {os.path.join(pypef_path, 'pypef', 'main.py')} "\
       f"encode -i {os.path.join(avgfp_path, 'avGFP.csv')} "\
       f"-e dca -w {os.path.join(avgfp_path, 'P42212_F64L.fasta')} "\
       f"--params {os.path.join(avgfp_path, 'uref100_avgfp_jhmmer_119_plmc_42.6.params')} "\
       f"--threads XX"
 
-print(os.cpu_count())
+print(f"Using up to {os.cpu_count()} cores/threads for parallel processing...")
 all_run_times = []
 for n_cores in range(1, os.cpu_count() + 1):
     run_time_1 = time.time()
-    print(f"Running command:\n============\n{cmd.replace('XX', str(n_cores))}")
+    print(f"Running command:\n================\n{cmd.replace('XX', str(n_cores))}")
     os.system(cmd.replace('XX', str(n_cores)))
     run_time_2 = time.time()
     all_run_times.append(run_time_2 - run_time_1)
