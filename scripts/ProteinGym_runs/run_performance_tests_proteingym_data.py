3170e86464a63f15390ec2e52cf86295a808270d
niklases <niklas.siedhoff@gmx.net>
2024-07-20 18:46:20 +0200

    Update adjust_text

--- scripts/ProteinGym_runs/run_performance_tests_proteingym_data.py
+++ scripts/ProteinGym_runs/run_performance_tests_proteingym_data.py
@@ -10,6 +10,7 @@ import time
 import psutil
 import gc
 from sklearn.model_selection import train_test_split
+from adjustText import adjust_text
 
 # Add local PyPEF path if not using pip-installed PyPEF version
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
@@ -18,9 +19,12 @@ from pypef.dca.hybrid_model import DCAHybridModel, get_delta_e_statistical_model
 from pypef.utils.variant_data import get_seqs_from_var_name
 
 
+MAX_WT_SEQUENCE_LENGTH = 100
 
-single_point_mut_data = os.path.abspath(os.path.join(os.path.dirname(__file__), f"single_point_dms_mut_data.json"))
-higher_mut_data = os.path.abspath(os.path.join(os.path.dirname(__file__), f"higher_point_dms_mut_data.json"))
+single_point_mut_data = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), f"single_point_dms_mut_data.json"))
+higher_mut_data = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), f"higher_point_dms_mut_data.json"))
 
 
 def plot_performance(mut_data, plot_name, mut_sep=':'):
@@ -28,8 +32,11 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
     dset_dca_perfs = []
     dset_hybrid_perfs = []
     n_tested_datasets = 0
+    plt.figure(figsize=(32, 12))
+    train_test_size_texts = []
     for i, (dset_key, dset_paths) in enumerate(mut_data.items()):
-        print(f'\n{i+1}/{len(mut_data.items())}\n===============================================================')
+        print(f'\n{i+1}/{len(mut_data.items())}\n'
+              f'===============================================================')
         csv_path = dset_paths['CSV_path']
         msa_path = dset_paths['MSA_path']
         wt_seq = dset_paths['WT_sequence']
@@ -38,16 +45,18 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
         wt_seq = wt_seq[msa_start - 1:msa_end]
         print('CSV path:', csv_path)
         print('MSA path:', msa_path)
-        print('MSA start:', msa_start)
+        print('MSA start:', msa_start, '- MSA end:', msa_end)
         print('WT sequence (trimmed from MSA start to MSA end):\n' + wt_seq)
         time.sleep(5)
         # Getting % usage of virtual_memory ( 3rd field)
-        print(f'RAM used: {round(psutil.virtual_memory()[3]/1E9, 3)} GB ({psutil.virtual_memory()[2]} %)')
+        print(f'RAM used: {round(psutil.virtual_memory()[3]/1E9, 3)} '
+              f'GB ({psutil.virtual_memory()[2]} %)')
         variant_fitness_data = pd.read_csv(csv_path, sep=',')
         print('N_variant-fitness-tuples:', np.shape(variant_fitness_data)[0])
-        if np.shape(variant_fitness_data)[0] > 400000:
-            print('More than 400000 variant-fitness pairs which is a potential OOM error risk, skipping dataset...')
-            continue
+        #if np.shape(variant_fitness_data)[0] > 400000:
+        #    print('More than 400000 variant-fitness pairs which represents a '
+        #          'potential out-of-memory risk, skipping dataset...')
+        #    continue
         variants = variant_fitness_data['mutant']
         fitnesses = variant_fitness_data['DMS_score']
         variants_split = []
@@ -59,8 +68,10 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
             wt_seq, variants_split, fitnesses, shift_pos=msa_start - 1)
         # Only model sequences with length of max. 800 amino acids to avoid out of memory errors 
         print('Sequence length:', len(wt_seq))
-        if len(wt_seq) > 1000:
-            print('Sequence length over 1000 which is a potential OOM error risk, skipping dataset...')
+        if len(wt_seq) > MAX_WT_SEQUENCE_LENGTH:
+            print(f'Sequence length over {MAX_WT_SEQUENCE_LENGTH}, which represents a potential out-of-memory risk '
+                  f'(when running on GPU, set threshold to length ~400 dependent on available VRAM), '
+                  f'skipping dataset...')
             continue
         gremlin_new = GREMLIN(alignment=msa_path, wt_seq=wt_seq, opt_iter=100, max_msa_seqs=10000)
         #gaps = gremlin_new.gaps
@@ -79,8 +90,9 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
         c = c / max_muts
         ratio_input_vars_at_gaps = c / len(var_pos)
         if c > 0:
-            print(f'{int(c)} of {len(var_pos)} ({ratio_input_vars_at_gaps * 100:.2f}%) input variants to be predicted are variants with '
-                  f'amino acid substitutions at gap positions (these variants will be predicted/labeled with a fitness of 0.0).')
+            print(f'{int(c)} of {len(var_pos)} ({ratio_input_vars_at_gaps * 100:.2f}%) input '
+                  f'variants to be predicted are variants with amino acid substitutions at gap' 
+                  f'positions (these variants will be predicted/labeled with a fitness of 0.0).')
         if ratio_input_vars_at_gaps >= 1.0:
             print('100% substitutions at gap sites, skipping dataset...')
             continue
@@ -89,34 +101,64 @@ def plot_performance(mut_data, plot_name, mut_sep=':'):
         x_wt = gremlin_new.x_wt
         # Statistical model performance
         y_pred = get_delta_e_statistical_model(x_dca, x_wt)
-        print(f'Statistical DCA model performance on all datapoints; Spearman\'s rho: {abs(spearmanr(fitnesses, y_pred)[0]):.3f}')
+        print(f'Statistical DCA model performance on all datapoints; Spearman\'s rho: '
+              f'{abs(spearmanr(fitnesses, y_pred)[0]):.3f}')
+        train_test_size_texts.append(plt.text(
+            n_tested_datasets, abs(spearmanr(fitnesses, y_pred)[0]), f'0'  + r'$\rightarrow$' + f'{len(fitnesses)}', size=4))
         assert len(x_dca) == len(fitnesses) == len(variants) == len(sequences)
         hybrid_perfs = []
-        for train_size in [0.01, 0.05, 0.1, 0.2, 0.3]:
-            x_train, x_test, y_train, y_test = train_test_split(x_dca, fitnesses, train_size=train_size, random_state=42)
-            hybrid_model = DCAHybridModel(x_train=x_train, y_train=y_train, x_wt=x_wt)
-            beta_1, beta_2, regressor = hybrid_model.settings(x_train=x_train, y_train=y_train)
-            y_test_pred = hybrid_model.hybrid_prediction(x=x_test, reg=regressor, beta_1=beta_1, beta_2=beta_2)
-            print(f'Hybrid DCA model performance on {len(y_test)} datapoints (train size: {train_size}. '
-                  f'N Train: {len(y_train)}, N Test: {len(y_test)}), Spearman\'s rho: {abs(spearmanr(y_test, y_test_pred)[0]):.3f}')
-            hybrid_perfs.append(abs(spearmanr(y_test, y_test_pred)[0]))
+        for train_size in [25, 50, 75, 100, 200]:
+            try:
+                x_train, x_test, y_train, y_test = train_test_split(
+                    x_dca, fitnesses, train_size=train_size, random_state=42)
+                hybrid_model = DCAHybridModel(x_train=x_train, y_train=y_train, x_wt=x_wt)
+                beta_1, beta_2, regressor = hybrid_model.settings(x_train=x_train, y_train=y_train)
+                y_test_pred = hybrid_model.hybrid_prediction(
+                    x=x_test, reg=regressor, beta_1=beta_1, beta_2=beta_2)
+                print(f'Hybrid DCA model performance on {len(y_test)} datapoints (Train size: {train_size}: '
+                      f'N Train: {len(y_train)}, N Test: {len(y_test)}). Spearman\'s rho: '
+                      f'{abs(spearmanr(y_test, y_test_pred)[0]):.3f}')
+                hybrid_perfs.append(abs(spearmanr(y_test, y_test_pred)[0]))
+                train_test_size_texts.append(plt.text(
+                    n_tested_datasets, abs(spearmanr(y_test, y_test_pred)[0]), f'{len(y_train)}'  + r'$\rightarrow$' + f'{len(y_test)}', size=4))
+            except ValueError:
+                hybrid_perfs.append(np.nan)
         n_tested_datasets += 1
-        tested_dsets.append(f'({n_tested_datasets}) {dset_key} ({100.0 - (ratio_input_vars_at_gaps * 100):.2f}%, {max_muts})')
+        tested_dsets.append(f'({n_tested_datasets}) {dset_key} '
+                            f'({100.0 - (ratio_input_vars_at_gaps * 100):.2f}%, {max_muts})')
         dset_dca_perfs.append(abs(spearmanr(fitnesses, y_pred)[0]))
         dset_hybrid_perfs.append(hybrid_perfs)
-        gc.collect()  # Potentially GC is needed to free some RAM (deallocated VRAM -> partly stored in RAM?) after run
-    plt.figure(figsize=(32, 12))
-    plt.plot(range(len(tested_dsets)), dset_dca_perfs, 'o--', markersize=8, label='DCA')
-    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 0], 'o--', markersize=8, label='Hybrid (0.01)')
-    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 1], 'o--', markersize=8, label='Hybrid (0.05)')
-    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 2], 'o--', markersize=8, label='Hybrid (0.1)')
-    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 3], 'o--', markersize=8, label='Hybrid (0.2)')
-    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 4], 'o--', markersize=8, label='Hybrid (0.3)') 
-    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.mean(dset_dca_perfs)), 'k--')
-    plt.text(len(tested_dsets) - 1, np.mean(dset_dca_perfs), f'{np.mean(dset_dca_perfs):.2f}')
+        #gc.collect()  # Potentially GC is needed to free some RAM (deallocated VRAM -> partly stored in RAM?) after run
+    plt.plot(range(len(tested_dsets)), dset_dca_perfs, 'o--', markersize=8, color='tab:blue', label='DCA')
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(dset_dca_perfs)), color='tab:blue', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(dset_dca_perfs), f'{np.nanmean(dset_dca_perfs):.2f}')
+
+    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 0], 'o--', markersize=8, color='tab:orange', label='Hybrid (25)')
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(np.array(dset_hybrid_perfs)[:, 0])), color='tab:orange', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(np.array(dset_hybrid_perfs)[:, 0]), f'{np.nanmean(np.array(dset_hybrid_perfs)[:, 0]):.2f}')
+
+    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 1], 'o--', markersize=8, color='tab:green', label='Hybrid (50)')
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(np.array(dset_hybrid_perfs)[:, 1])), color='tab:green', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(np.array(dset_hybrid_perfs)[:, 1]), f'{np.nanmean(np.array(dset_hybrid_perfs)[:, 1]):.2f}')
+
+    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 2], 'o--', markersize=8, color='tab:red',  label='Hybrid (75)')
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(np.array(dset_hybrid_perfs)[:, 2])), color='tab:red', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(np.array(dset_hybrid_perfs)[:, 2]), f'{np.nanmean(np.array(dset_hybrid_perfs)[:, 2]):.2f}')
+
+    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 3], 'o--', markersize=8, color='tab:purple', label='Hybrid (100)')
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(np.array(dset_hybrid_perfs)[:, 3])), color='tab:purple', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(np.array(dset_hybrid_perfs)[:, 3]), f'{np.nanmean(np.array(dset_hybrid_perfs)[:, 3]):.2f}')
+
+    plt.plot(range(len(tested_dsets)), np.array(dset_hybrid_perfs)[:, 4], 'o--', markersize=8, color='tab:brown', label='Hybrid (200)') 
+    plt.plot(range(len(tested_dsets)), np.full(np.shape(tested_dsets), np.nanmean(np.array(dset_hybrid_perfs)[:, 4])), color='tab:brown', linestyle='--')
+    plt.text(len(tested_dsets) - 1, np.nanmean(np.array(dset_hybrid_perfs)[:, 4]), f'{np.nanmean(np.array(dset_hybrid_perfs)[:, 4]):.2f}')
+
+
     plt.xticks(range(len(tested_dsets)), tested_dsets, rotation=45, ha='right')
+    plt.margins(0.01)
     plt.legend()
     plt.tight_layout()
+    adjust_text(train_test_size_texts, only_move={'points': 'y', 'text': 'y'}, force_points=0.5)
     plt.ylim(0.0, 1.0)
     plt.ylabel(r'|Spearmanr $\rho$|')
     plt.savefig(os.path.join(os.path.dirname(__file__), f'{plot_name}.png'), dpi=300)
