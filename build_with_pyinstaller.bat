0ba3b422559237a0ef112143f0ca3e6c171e6f66
niklases <niklas.siedhoff@gmx.net>
2025-05-06 00:48:11 +0200

    Update WIndows PyInstaller build: include DLLs

--- build_with_pyinstaller.bat
+++ build_with_pyinstaller.bat
@@ -1,4 +1,5 @@
-conda install cython
+REM Up to now pastes DLLs from local Python environment bin's to _internal...
+REM alternative?: set PATH=%PATH%;c:\Users\nikla\miniconda3\envs\py312\Library\bin\;
 pip install -U pyinstaller
 pip install -e .
 pyinstaller^
@@ -10,5 +11,11 @@ pyinstaller^
   --collect-data torch_geometric^
   --collect-all torch_geometric^
   --hidden-import torch_geometric^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\onedal_thread.3.dll:.^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\tbbbind.dll:.^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\tbbbind_2_0.dll:.^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\tbbbind_2_5.dll:.^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\tbbmalloc.dll:.^
+  --add-binary=c:\Users\nikla\miniconda3\envs\py312\Library\bin\tbbmalloc_proxy.dll:.^
   --noconfirm^
   gui\qt_window.py
