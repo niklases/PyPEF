edef1f299be1aab917d3562fe6f96c99c8f28bb2
niklases <niklas.siedhoff@gmx.net>
2025-05-05 18:56:24 +0200

    Update PyInstaller build

--- build_with_pyinstaller.sh
+++ build_with_pyinstaller.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+pip install -e .
 pyinstaller \
   --console \
-  --collect-data torch\
-  --collect-data biotite\
-  --collect-all biotite\
-  --collect-data torch_geometric\
-  --collect-all torch_geometric\
-  --hidden-import torch_geometric\
+  --collect-all pypef \
+  --collect-data torch \
+  --collect-data biotite \
+  --collect-all biotite \
+  --collect-data torch_geometric \
+  --collect-all torch_geometric \
+  --hidden-import torch_geometric \
+  --noconfirm \
   gui/qt_window.py
   #  --add-data=X/:X/. \
