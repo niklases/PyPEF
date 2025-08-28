d78e371e913af3acfdc673f602821a7fa6acd520
niklases <niklas.siedhoff@gmx.net>
2025-08-28 21:56:40 +0200

    Update get_py_packages.sh

--- scripts/ProteinGym_runs/official/get_py_packages.sh
+++ scripts/ProteinGym_runs/official/get_py_packages.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/niklases/PyPEF/archive/refs/heads/dev-v0.4.3.zip
-unzip dev-v0.4.3.zip && rm dev-v0.4.3.zip
-cd PyPEF-dev-v0.4.3/
+gh repo clone niklases/pypef
+cd PyPEF
 pip install -r requirements.txt
 pip install hydra-core
 pip install .
-
-
